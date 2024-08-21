a19091e3d19d61193c6abb6d2493ab098a335d79
Sebastian Ireland <irelandsh10@gmail.com>
2024-08-21 11:02:21 -0400

    commit

--- app/view/mantenedor_usuario.php
+++ app/view/mantenedor_usuario.php
@@ -57,114 +57,94 @@
         </nav>
     </header>
     <main>
-        <!-- Contenido main -->
         <div class="container-fluid">
-            <main>
-                <div class="row my-3">
-                    <div id="carouselExample" class="carousel carousel-dark slide" data-bs-interval="false">
-                        <div class="carousel-inner">
-                            <ul class="nav nav-pills" id="pills-tab" role="tablist">
-                                <div class="carousel-item active">
-                                    <div class="row g-3 justify-content-center">
-                                        <div class="col-auto d-flex justify-content-center">
-                                            <div class="card shadow-sm hover-effect" style="width:150px; height:75px;">
-                                                <div class="card-body d-flex align-items-center stylecard">
-                                                    <div class="me-3">
-                                                        <i class="bi bi-book" style="font-size: 1.5rem;"></i>
-                                                    </div>
-                                                    <div>
-                                                        <h6 class="card-title mb-1">Registrar usuarios</h6>
-                                                        <p class="mb-0 " style="font-size: 0.875rem;"></p>
-                                                    </div>
-                                                    <a href="#datos-academicos" id="llamar-datos-academicos"
-                                                        class="stretched-link active" id="datos-academicos-tab"
-                                                        data-bs-toggle="tab" data-bs-target="#datos-academicos"
-                                                        type="button" role="tab" aria-controls="academicos-tab-pane"
-                                                        aria-selected="true"></a>
+            <div class="row my-3">
+                <div id="carouselExample" class="carousel carousel-dark slide" data-bs-interval="false">
+                    <div class="carousel-inner">
+                        <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                            <div class="carousel-item active">
+                                <div class="row g-3 justify-content-center">
+                                    <div class="col-auto d-flex justify-content-center">
+                                        <div class="card shadow-sm hover-effect" style="width:150px; height:75px;">
+                                            <div class="card-body d-flex align-items-center stylecard">
+                                                <div class="me-3">
+                                                    <i class="bi bi-book" style="font-size: 1.5rem;"></i>
                                                 </div>
+                                                <div>
+                                                    <h6 class="card-title mb-1">Registrar usuarios</h6>
+                                                    <p class="mb-0 " style="font-size: 0.875rem;"></p>
+                                                </div>
+                                                <a href="#datos-academicos" id="llamar-datos-academicos"
+                                                    class="stretched-link active" id="datos-academicos-tab"
+                                                    data-bs-toggle="tab" data-bs-target="#datos-academicos"
+                                                    type="button" role="tab" aria-controls="academicos-tab-pane"
+                                                    aria-selected="true"></a>
                                             </div>
                                         </div>
-                                        <div class="col-auto d-flex justify-content-center">
-                                            <div class="card shadow-sm hover-effect" style="width:150px; height:75px;">
-                                                <div class="card-body d-flex align-items-center stylecard">
-                                                    <div class="me-3">
-                                                        <i class="bi-pencil" style="font-size: 1.5rem;"></i>
-                                                    </div>
-                                                    <div>
-                                                        <h6 class="card-title mb-1">Gestionar roles</h6>
-                                                        <p class="mb-0 " style="font-size: 0.875rem;"></p>
-                                                    </div>
-                                                    <a href="#datos-matriculas" id="llamar-datos-matriculas"
-                                                        class="stretched-link" data-bs-toggle="tab"
-                                                        data-bs-target="#datos-matriculas" type="button" role="tab"
-                                                        aria-controls="matriculas-tab-pane" aria-selected="false"></a>
+                                    </div>
+                                    <div class="col-auto d-flex justify-content-center">
+                                        <div class="card shadow-sm hover-effect" style="width:150px; height:75px;">
+                                            <div class="card-body d-flex align-items-center stylecard">
+                                                <div class="me-3">
+                                                    <i class="bi-pencil" style="font-size: 1.5rem;"></i>
                                                 </div>
+                                                <div>
+                                                    <h6 class="card-title mb-1">Gestionar roles</h6>
+                                                    <p class="mb-0 " style="font-size: 0.875rem;"></p>
+                                                </div>
+                                                <a href="#datos-matriculas" id="llamar-datos-matriculas"
+                                                    class="stretched-link" data-bs-toggle="tab"
+                                                    data-bs-target="#datos-matriculas" type="button" role="tab"
+                                                    aria-controls="matriculas-tab-pane" aria-selected="false"></a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </ul>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-auto">
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="datos-academicos" role="tabpanel"
+                            aria-labelledby="home-tab" tabindex="0">
+                            <div class="card">
+                                <div class="card-header bg-primary">
+                                    <h6 class="text-wrap text-light mt-2">Tabla Usuarios</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col table-responsive">
+                                            <table
+                                                id="tabla-usuario" class="table table-hover table-striped text-wrap w-100">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Rut</th>
+                                                        <th>Usuario</th>
+                                                        <th>Fecha Inicio</th>
+                                                        <th>Fecha Termino</th>
+                                                        <th>Roles</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody></tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="datos-matriculas" role="tabpanel"
+                            aria-labelledby="profile-tab" tabindex="0">
+                            <?php include_once("layouts/rol/mantenedor_rol.php"); ?>
+                        </div>
+                        <div class="tab-pane fade" id="datos-bienestar" role="tabpanel"
+                            aria-labelledby="contact-tab" tabindex="0">
                         </div>
                     </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-auto">
-                                <div class="tab-content" id="myTabContent">
-                                    <div class="tab-pane fade show active" id="datos-academicos" role="tabpanel"
-                                        aria-labelledby="home-tab" tabindex="0">
-                                        <div class="container-fluid">
-                                            <!-- Contenedor principal -->
-                                            <div class="row">
-                                                <div class="col">
-                                                    <!-- Aca iria la toolvar para gestionar las variables de la tabla -->
-                                                    <div class="row">
-                                                        <div class="col"></div>
-                                                    </div>
-                                                    <!-- Aca se cargara la tabla con los datos del usuario -->
-                                                    <div class="card">
-                                                        <div class="card-header bg-primary">
-                                                            <h6 class="text-wrap text-light mt-2">Tabla Usuarios</h6>
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <div class="row">
-                                                                <div class="col table-responsive">
-                                                                    <table
-                                                                        class="table table-hover table-striped text-wrap w-100 tablita">
-                                                                        <thead>
-                                                                            <tr>
-                                                                                <th>Usuario</th>
-                                                                                <th>Rut</th>
-                                                                                <th>Apellido paterno</th>
-                                                                                <th>Apellido Materno</th>
-                                                                                <th>Fecha Inicio</th>
-                                                                                <th>Fecha Termino</th>
-                                                                                <th>Roles</th>
-                                                                                <th>Acciones</th>
-                                                                            </tr>
-                                                                        </thead>
-                                                                        <tbody></tbody>
-                                                                    </table>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <!-- Aca va la card con las demas vistas -->
-                                                <!-- <div class="col"></div> -->
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="tab-pane fade" id="datos-matriculas" role="tabpanel"
-                                        aria-labelledby="profile-tab" tabindex="0">
-                                        <?php include_once("layouts/rol/mantenedor_rol.php"); ?>
-                                    </div>
-                                    <div class="tab-pane fade" id="datos-bienestar" role="tabpanel"
-                                        aria-labelledby="contact-tab" tabindex="0">
-                                    </div>
-                                </div>
-                    </div>
-                </div>
-            </main>
+            </div>
             <!-- Footer contenido -->
             <footer></footer>
         </div>
@@ -173,13 +153,13 @@
 </body>
 <script defer src="assets/js//datatable.js"></script>
 <script defer src="assets/js//get_rol.js"></script>
-<script>
-    $(document).ready(function () {
+<!-- <script>
+    $(document).ready(function() {
         $.ajax({
             url: 'https://portalonlinedev.unap.cl/MantenedoresSat/presentacion/index.php?caso=usuarios_registrados',
             dataType: 'JSON',
             method: 'GET',
-            success: function (response) {
+            success: function(response) {
                 const usuarios = response.datosTabla;
                 const tablita = $('.tablita tbody');
                 let filas = '';
@@ -197,16 +177,61 @@
                     </tr>
                 `;
                     });
-                // Object.entries(usuarios).forEach(([rut, usuario]) => {
-                //     const roles = usuario.descroles.length ? usuario.descroles.join(', ') : 'S/R';
-                // });
 
-                tablita.append(filas); // Añade todas las filas al DOM en una sola operación
+
+                tablita.append(filas);
             },
-            error: function (error) {
+            error: function(error) {
                 console.error("Error:", error);
             },
         });
+    });
+</script> -->
+<script>
+    new DataTable("#tabla-usuario", {
+        layout: {
+            topStart: "buttons",
+        },
+        dom: "Bfrtip",
+        buttons: [{
+            extend: "copy",
+            className: "btn-primary",
+            text: '<i class="bi bi-clipboard"></i>',
+        }, ],
+        bottomEnd: {
+            paging: {
+                firstLast: false,
+            },
+        },
+        ajax: {
+            url: 'https://portalonlinedev.unap.cl/MantenedoresSat/presentacion/index.php?caso=usuarios_registrados',
+            method: 'POST',
+            dataSrc: 'datosTabla',
+
+        },
+        columns: [{
+                data: 'run'
+            },
+            {
+                data: 'nombres'
+            },
+            {
+                data: 'fechainicio'
+            },
+            {
+                data: 'fechafin'
+            },
+            {
+                data: 'descroles'
+            },
+
+        ],
+
+        searching: true,
+        ordering: true,
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json",
+        },
     });
 </script>
 
